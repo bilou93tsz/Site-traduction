c154f562c33e0080afae10abaed9ceb680d32b8e
bilou93tsz <bilou93tsz@gmail.com>
2013-09-26 09:12:49 +0000

    page2.php

---
+++ page2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html/>
+<html>
+	<head>
+		<meta content=" text/javascript ; charset=UTF-8"/>
+		<link href="style.css" type="text/css" rel="stylesheet"/>
+		<script type="text/javascript" src='jquery-1.9.1.js'></script>
+
+ <script type="text/javascript" src='script2.js'></script>
+		
+ </head>
+<body>
+	<div id='banniere'>
+		<img id='logo' src='wiki-logo.png'/>
+		<br/>
+		<img id='img' src='img2.jpg'/>
+	 </div>
+	 <a id='warp' href='page1.php'> Latin ==> Français </a>
+	<div id='act'>
+		<textarea rows=3 cols=15></textarea>
+		<button>
+			Traduire
+		</button>
+		<div id='trad'>
+		 </div>
+</div>
+ 
+
+	 </body>
+ </html>
