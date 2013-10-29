caa5696b5f054b11a1120fedda3eee3dfe8183f7
naimakotbi <kotbi.naima@gmail.com>
2013-10-29 16:04:18 +0100

    Create terminaisonAnglaise.php

---
+++ terminaisonAnglaise.php
@@ -0,0 +1,55 @@
+
+
+<?php 
+
+$persEn = array(
+
+"persEn1" => "I",
+
+"persEn2" => "You",
+
+"persEn3" => "He\She\It",
+
+"persEn4" => "We",
+
+"persEn5" => "You",
+
+"persEn6" => "they"
+
+
+
+);
+
+
+
+
+
+$groupeReg = array(
+
+"present1" => "",
+
+"present2" => "",
+
+"present3" => "s",
+
+"present4" => "",
+
+"present5" => "",
+
+"present6" => "",
+
+"imparfait1" => "ed",
+
+"imparfait2" => "ed",
+
+"imparfait3" => "ed",
+
+"imparfait4" => "ed",
+
+"imparfait5" => "ed",
+
+"imparfait6" => "ed",
+
+);
+
+?>
