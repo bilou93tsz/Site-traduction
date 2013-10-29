11025debfd589b03770c5c7ddc2fc0885f0f63f1
naimakotbi <kotbi.naima@gmail.com>
2013-10-29 16:03:24 +0100

    Create terminaisonFrancaise.php

---
+++ terminaisonFrancaise.php
@@ -0,0 +1,111 @@
+
+
+
+
+                                                                                                                                                                                                             <?php 
+
+$personne = array(
+
+"pers1" => "Je",
+
+"pers2" => "Tu",
+
+"pers3" => "Il\Elle\On",
+
+"pers4" => "Nous",
+
+"pers5" => "Vous",
+
+"pers6" => "Ils\Elles"
+
+
+
+);
+
+
+
+
+
+$groupe1 = array(
+
+"present1" => "e",
+
+"present2" => "es",
+
+"present3" => "e",
+
+"present4" => "ons",
+
+"present5" => "ez",
+
+"present6" => "ent",
+
+"imparfait1" => "ais",
+
+"imparfait2" => "ais",
+
+"imparfait3" => "ait",
+
+"imparfait4" => "ions",
+
+"imparfait5" => "iez",
+
+"imparfait6" => "aient",
+
+"futur1" => "erai",
+
+"futur2" => "eras",
+
+"futur3" => "era",
+
+"futur4" => "erons",
+
+"futur5" => "erez",
+
+"futur6" => "eront"
+
+);
+
+
+
+$groupe2 = array(
+
+"present1" => "is",
+
+"present2" => "is",
+
+"present3" => "it",
+
+"present4" => "issons",
+
+"present5" => "issez",
+
+"present6" => "issent",
+
+"imparfait1" => "issais",
+
+"imparafit2" => "issais",
+
+"imparfait3" => "issait",
+
+"imparfait4" => "issions",
+
+"imparfait5" => "issiez",
+
+"imparfait6" => "issaient",
+
+"futur1" => "irai",
+
+"futur2" => "iras",
+
+"futur3" => "ira",
+
+"futur4" => "irons",
+
+"futur5" => "irez",
+
+"futur6" => "iront"
+
+);
+
+?>
