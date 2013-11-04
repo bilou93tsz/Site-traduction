ebd609a560e35d3108fac73ca19de28de471ea2a
naimakotbi <kotbi.naima@gmail.com>
2013-11-04 22:07:47 +0100

    Create gestionMot.php

---
+++ gestionMot.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+
+        <head>
+
+               
+
+		<meta http-equiv="content-type" content="text/html" charset="utf-8" /> 
+
+                <link href="style.css" type="text/css" rel="stylesheet"/>
+
+		<link rel="shortcut icon" href="images/wiki-logo.ico">
+
+                <script type="text/javascript" src='jquery-1.9.1.js'></script>
+
+		<script type="text/javascript" src='script.js'></script>
+
+		<title> Traducteur </title>
+
+	</head>
+
+ 
+
+	<body>
+
+
+
+        	<div id='banniere'>
+
+                	<a href='accueil.php'><img id='img' src='images/img1.jpg'/></a>
+
+        	</div>
+
+
+
+		<h1> Ajouter un mot: </h1>
+
+		   
+
+		  <p> <ul> 
+
+			 <li> <a href='ajouterNom.php'> Ajouter un nom  </li><br>
+
+			<li> <a href='ajouterVerbe.php'> Ajouter un verbe  </li><br>
+
+			<li> <a href='ajouterMotInvariable.php'> Ajouter un mot invariable </li><br>
+
+			<li> <a href='ajouterAdjectif.php'> Ajouter un adjectif </li><br>
+
+		   </ul>
+
+		</p>
+
+		
+
+	</body>
+
+			
