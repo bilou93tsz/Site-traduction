a59f3dd4f0fae97248352b6e37756e5d40a65187
ahamadaali <equipewiki@gmail.com>
2013-11-06 00:23:16 +0100

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -14,9 +14,18 @@
 				$bdd->query('DELETE FROM Utilisateurs WHERE login='.$nom);
 			}
 			$bdd->query('UPDATE Utilisateurs SET droit='.$var.' WHERE login='.$nom);
-		} 	
+		}
 	}
 	$reponse->closeCursor();
+
+	$reponse1=$bdd->query('SELECT numero FROM Regle');
+	while($num = $reponse1->fetch()){
+		if(isset($_POST[$num])){
+			$var=$_POST[$num];
+			$bdd->query($var.'Regle SET regleFR='.$POST['regle1'].' regleEN='.$POST['regle2'].' WHERE numero='.$num);
+		}
+	}
+	
 ?>
 	<div id="Connexion" class='admin'>
 		<span> Salut </span><br/>
@@ -44,16 +53,29 @@
 		<h2 style="texte-decoration:underline;">Gérer Règles :</h2>
 		
 		<div id='grammaire'style="border:1px solid;height:150px;width:500px;overflow:auto;">
-<?php	$reponse = $bdd->query('SELECT regleFR,regleEN FROM Regle');
+<?php	$reponse = $bdd->query('SELECT numero FROM Regle');
+	$reponse1 = $bdd->query('SELECT regleFR FROM Regle');
+	$reponse2 = $bdd->query('SELECT regleEN FROM Regle');
 ?>
         <form action="Admin.php" method="post">
-        
-			</form>
+        	<?php while ($regleFR = $reponse1->fetch() && $regleEN=$reponse2->fetch() && $num=$reponse->fetch()){
+		echo"<input type='text' name='regle1' value='".$regleFR."' />
+		<input type='text' name='regle2' value='".$regleEN."' />
+		<input type='submit' name='".$num."' value='UPDATE' />
+		<input type='submit' name='".$num."' value='DELETE' />
+		<hr/>";	
+		}
+		$reponse->closeCursor();
+?>
+
+	</form>
 		</div>
 		
 		<h2 style="texte-decoration:underline;">Gérer Mots :</h2>
         <form action="Admin.php" method="post">
-			</form>
+        	<?php 
+        	?>
+	</form>
 			
 		</div>
 	</div>
