9f0aeee4a2e174033776cccdc759f81a37c7e835
sihn92 <mesbahi.amjed@yahoo.fr>
2013-11-06 00:56:01 +0100

    Create accueilVip.php

---
+++ accueilVip.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title>Page accueil</title>
+
+<link href="accueilVip.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<h2>Vous etes connect en tant qu'utilisateur VIP</h2>
+
+<div id='banniere'>
+
+                <img id='logo' src='Wiki_logo.png'/>
+
+</div>
+
+<p>Voici les differentes options qui s'offrent a vous</p>
+
+
+
+<table>
+
+	<tr><td><a href="ajouterregles.php"> <input type="button" value="Ajouter une regle"> </a></form></td>
+
+	    <td><p>Ici vous pouvez ajouter une regle grammatical en francais et en anglais pour parfaire les traductions donnees par le site</p></td></tr>
+
+	<tr><td><a href="ajouterM.php"> <input type="button" value="Ajouter un mot"> </a></td>
+
+	    <td><p>Ici vous pouvez ajouter un mot en francais et en anglais pour parfaire les traductions donnees par le site</p></td></tr>
+
+	<tr><td><a href="page1.php"> <input type="button" value="Se deconnecter"> </a></td>
+
+	    <td><p>Ici vous pouvez vous deconnecter</p></td></tr>
+
+</table>
+
+</body>
+
+</html>
